997067ad4f8df9b0d67112ed42bc653bf94072be
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-12-05 15:35:53 +0800

    Fix StudentService environment

--- src/app/shared/services/student.service.ts
+++ src/app/shared/services/student.service.ts
@@ -1,4 +1,4 @@
-import { environment } from './../../../environments/environment.prod';
+import { environment } from './../../../environments/environment';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
