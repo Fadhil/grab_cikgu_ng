5fea6eb29ee663e2454c7f4cebf59db30f33765e
Unknown <izzah@flexcility.com>
2018-01-12 10:00:35 +0800

    Display tutor's details - search page
    - added age, gender, city, qualification, occupation, experience

--- src/app/shared/services/firebase.service.ts
+++ src/app/shared/services/firebase.service.ts
@@ -56,7 +56,17 @@ export class FirebaseService {
       for (let subject of tutor.subjects) {
         for (let x = 0; x < subject.levels.length; x++) {
           if (subject.levels[x]) {
-            newTutor['/Location/' + tutor.city + '/' + subject.name + '/levels/' + x + '/' + tutor.id] = {name: tutor.name, email: tutor.email, rate: tutor.hourly_rate_cents? tutor.hourly_rate_cents:0};
+            newTutor['/Location/' + tutor.city + '/' + subject.name + '/levels/' + x + '/' + tutor.id] = {name: tutor.name,
+                                                                                                          email: tutor.email,
+                                                                                                          occupation: tutor.occupation,
+                                                                                                          qualification: tutor.qualification,
+                                                                                                          city: tutor.city,
+                                                                                                          achievement: tutor.achievement,
+                                                                                                          gender: tutor.gender,
+                                                                                                          age: tutor.age,
+                                                                                                          
+                                                                                                          experience: tutor.experience,
+                                                                                                          rate: tutor.hourly_rate_cents? tutor.hourly_rate_cents:0};
           } else {
             newTutor['/Location/' + tutor.city + '/' + subject.name + '/levels/' + x + '/' + tutor.id] = null;
           }
