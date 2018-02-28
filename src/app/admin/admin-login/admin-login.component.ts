5e3d72d89a8bf06e1996292bc2e2b1e928a5f9cb
Hazim <hazim@p2digital.com>
2018-02-28 17:55:22 +0800

    admin

--- src/app/admin/admin-login/admin-login.component.ts
+++ src/app/admin/admin-login/admin-login.component.ts
@@ -27,25 +27,26 @@ export class AdminLoginComponent implements OnInit {
 
   login() {
 
-    // Check whether this is an admin or not?
-          // console.log("it's an admin");
     this.firebaseService.login(this.email, this.password)
       .then( firebaseUser => {
         console.log(firebaseUser);
         localStorage.setItem('currentUserToken', firebaseUser.uid);
         localStorage.setItem('loginType', 'admin');
-        // this.firebaseService.getAdmin(firebaseUser.uid);
-          // .subscribe(data => {
-          //
-          // });
-          this.firebaseService.checkAdmin(this.email)
+
+          let s_observable = this.firebaseService.checkAdmin(this.email)
             .subscribe(res => {
               if (res.length > 0) {
                 location.assign('/admin/students');
                 this.alertService.success('Successfully logged in', true);
               } else {
-                this.alertService.success("Sorry buddy, you're not a admin", true);
+                this.alertService.error("Sorry buddy, you're not a admin", true);
                 //perform firebase logout
+
+                s_observable.unsubscribe();
+                this.firebaseService.logout().then(result => {
+                  // this.alertService.success('Successfully Logged Out from GrabCikgu.');
+                });
+
               }
             });
       })
