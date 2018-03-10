178385b29fd52e4ddb6d2b57549437afe7de1ae0
Hazim <hazim@p2digital.com>
2018-03-10 18:01:48 +0800

    admin student booking tests

--- e2e/admin/confirm-booking-spec.ts
+++ e2e/admin/confirm-booking-spec.ts
@@ -1,7 +1,24 @@
 var helper = require('./helper.ts');
+var Promise = require('bluebird');
 
 describe('TutorGo Admin confirm booking', function() {
-  beforeAll( () => {
+  // var originalTimeout;
+  //
+  // beforeEach(function() {
+  //     originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+  //     // console.log(originalTimeout);
+  //     jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+  // });
+  //
+  // afterEach(function() {
+  //   jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+  // });
+
+
+
+
+  beforeAll( (done) => {
+    // protractor.driver.manage().timeouts().setScriptTimeout(60000);
     console.log("Before All");
     const Faker = require('faker');
     browser.params.randomName = Faker.name.findName();
@@ -12,12 +29,94 @@ describe('TutorGo Admin confirm booking', function() {
     browser.params.studentEmail2 = Faker.internet.email();
     browser.params.tutorName = Faker.name.findName();
     browser.params.tutorEmail = Faker.internet.email();
+
+    browser.wait(helper.RegisterStudent(browser.params.studentName1, browser.params.studentEmail1), 5000)
+      .then(()=>{
+        browser.wait(helper.RegisterStudent(browser.params.studentName2, browser.params.studentEmail2), 5000)
+          .then(()=>{
+            browser.wait(helper.RegisterTutor(browser.params.tutorName, browser.params.tutorEmail), 5000)
+              .then(()=>{
+                browser.wait(helper.TutorRegisterSubject(browser.params.tutorName, browser.params.tutorEmail), 30000)
+                  .then(()=>{
+                    console.log("Actually done");
+                    done();
+                  });
+              });
+          });
+      });
+
+
+    // (new Promise(function (resolve) {
+    //       setTimeout(resolve.bind(null, 'foo'), 60000);
+    //
+    //     })
+    //     .then(function(){
+    //       console.log('Calling done');
+    //       done();
+    //     })
+    //   ,60000);
+
+    // browser.wait(helper.RegisterStudent(browser.params.studentName1, browser.params.studentEmail1), 5000)
+    //   .then(()=>{
+    //     browser.wait(helper.RegisterStudent(browser.params.studentName2, browser.params.studentEmail2), 5000)
+    //       .then(()=>{
+    //         browser.wait(helper.RegisterTutor(browser.params.tutorName, browser.params.tutorEmail), 5000)
+    //           .then(()=>{
+    //             console.log("Actually done");
+    //           });
+    //       })
+    //   });
+
+
+    // browser.wait(helper.RegisterTutor(browser.params.tutorName, browser.params.tutorEmail), 5000);
+    // var myRegisterTutor = new helper.RegisterTutor(browser.params.tutorName, browser.params.tutorEmail, done);
+    // browser.wait(myHelper2, 5000);
+
+    // done();
+      // .then(function(){
+      //   console.log("Executed");
+      //   done();
+      //   var myRegisterStudent2 = new helper.RegisterStudent(browser.params.studentName2, browser.params.studentEmail2)
+      //       .then(result => {
+      //         console.log(result);
+      //         done();
+      //         var myRegisterStudent3 = new helper.RegisterStudent(browser.params.tutorName, browser.params.tutorEmail)
+      //             .then(result => {
+      //               console.log("Created Tutor executed value");
+      //               console.log(result);
+      //               done();
+      //             })
+      //             .catch(error => {
+      //               console.log(error);
+      //             });
+      //       }).catch(error => {
+      //         console.log(error);
+      //       });
+      // })
+      // .catch(error => {
+      //   console.log(error);
+      // });
+
+    // var myRegisterTutor = new helper.RegisterTutor(browser.params.tutorName, browser.params.tutorEmail, done);
+    // expect(myHelper.state).toBe('Executed');
+
     //Create 2 students here
-    helper.RegisterStudent(browser.params.studentName1, browser.params.studentEmail1);
-    helper.RegisterStudent(browser.params.studentName2, browser.params.studentEmail2);
-    helper.RegisterTutor(browser.params.tutorName, browser.params.tutorEmail);
-    helper.TutorRegisterSubject(browser.params.tutorName, browser.params.tutorEmail);
-  });
+    // myHelper.execute(function(){
+    //   console.log("Execute function");
+    //   expect(myHelper.state).toBe('Executed');
+    //   console.log("execute done");
+    //   var myRegisterStudent2 = new helper.RegisterStudent(browser.params.studentName2, browser.params.studentEmail2, done);
+    //   console.log("execute student 2");
+    //   done();
+    // });
+
+    // done();
+    // helper.RegisterStudent(browser.params.studentName1, browser.params.studentEmail1);
+    // helper.RegisterStudent(browser.params.studentName2, browser.params.studentEmail2);
+    // helper.RegisterTutor(browser.params.tutorName, browser.params.tutorEmail);
+    // helper.TutorRegisterSubject(browser.params.tutorName, browser.params.tutorEmail, done);
+    // done();
+  }, 120000);
 
   it('as an admin i would like to view the list of students request bookings', function() {
     console.log("as an admin I would like to view the list of students request bookings");
@@ -33,12 +132,13 @@ describe('TutorGo Admin confirm booking', function() {
     //     return url === 'http://localhost:4200/admin/bookings';
     //   });
       //pending();
-    }
+      // done();
+    }, 60000
   );
 
   it('should logout successfully', function() {
     //pending();
-  });
+  }, 60000);
 
   it('should not allow unauthorized access', function() {
   //   browser.get('http://localhost:4200/admin/login');
@@ -51,4 +151,4 @@ describe('TutorGo Admin confirm booking', function() {
   //   expect(element(by.css('.alert')).getText()).toContain('Error: The password is invalid or the user does not have a password.');
     //pending();
   });
-});
+}, 60000);
--- e2e/admin/helper.ts
+++ e2e/admin/helper.ts
@@ -5,7 +5,7 @@ function writeScreenShot(data, filename) {
     stream.end();
 }
 
-var urlChanged = function (testUrl) {
+var urlChanged = function(testUrl) {
   return browser.getCurrentUrl().then(function(url) {
     return url === testUrl;
   });
@@ -13,9 +13,14 @@ var urlChanged = function (testUrl) {
 
 //module.exports.urlChanged = urlChanged;
 
-var RegisterStudent = function (name, email) {
+var RegisterStudent = function(name, email) {
+  var deferred = protractor.promise.defer();
   browser.get('http://localhost:4200/student/register');
-  expect(element(by.id('register-form-link')).getText()).toContain('Register');
+  browser.wait(function() {
+    return element(by.id('register-form-link')).isPresent();
+  }, 5000);
+
+  // expect(element(by.id('register-form-link')).getText()).toContain('Register');
   element(by.id('name')).sendKeys(name);
   element(by.id('email')).sendKeys(email);
   element(by.id('password')).sendKeys('12345678');
@@ -26,13 +31,16 @@ var RegisterStudent = function (name, email) {
     return element(by.css('.alert')).isPresent();
   }, 5000);
   expect(element(by.css('.alert')).getText()).toContain("Successfully registered as a Student.");
-  expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/student/login');
+  // expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/student/login');
   console.log("Created Student");
+  deferred.fulfill(true);
+  return deferred.promise;
 };
 
 //module.exports.RegisterStudent = RegisterStudent;
 
-var RegisterTutor = function (name, email) {
+var RegisterTutor = function(name, email) {
+  var deferred = protractor.promise.defer();
   browser.get('http://localhost:4200/tutor/register');
   expect(element(by.id('register-form-link')).getText()).toContain('Register');
   element(by.id('name')).sendKeys(name);
@@ -59,11 +67,19 @@ var RegisterTutor = function (name, email) {
   expect(element(by.css('.alert')).getText()).toContain("Successfully registered as a Tutor.");
   expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/tutor/login');
   console.log("Created Tutor");
+
+  deferred.fulfill(true);
+  return deferred.promise;
 };
 
 //module.exports.RegisterTutor = RegisterTutor;
 
-var TutorRegisterSubject = function (name, email){
+var TutorRegisterSubject = function(name, email){
+  console.log("TutorRegisterSubject");
+  var deferred = protractor.promise.defer();
+  originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+  console.log("Original Timeout");
+  console.log(originalTimeout);
   browser.get('http://localhost:4200/tutor/login');
   element(by.id('email')).sendKeys(email);
   element(by.id('password')).sendKeys('12345678');
@@ -77,22 +93,21 @@ var TutorRegisterSubject = function (name, email){
     });
   };
 
-
   browser.wait(urlChanged, 5000);  //wait up to 60 seconds for URL to change
 
   browser.wait(function() {
     //wait for another 30 seconds
     return element(by.id('profilepagetitle')).isPresent();
   }, 10000);
-
-  browser.takeScreenshot().then(function (png) {
-          writeScreenShot(png, 'exception.png');
-      });
-  // expect(element(by.id('profilepagetitle')).getText()).toContain('Profile');
-  // click on the button
-
+  //
+  // browser.takeScreenshot().then(function (png) {
+  //         writeScreenShot(png, 'exception.png');
+  //     });
+  // // expect(element(by.id('profilepagetitle')).getText()).toContain('Profile');
+  // // click on the button
+  //
   element(by.id('editprofilebutton')).click();
-
+  //
   urlChanged = function(dom) {
     return browser.getCurrentUrl().then(function(url) {
       return url === 'http://localhost:4200/tutor/profile/edit';
@@ -100,6 +115,12 @@ var TutorRegisterSubject = function (name, email){
   };
 
   browser.wait(urlChanged, 5000);
+
+  browser.wait(function() {
+    //wait for another 30 seconds
+    return element(by.id('profileinformation')).isPresent();
+  }, 10000);
+
 
   // change some data
   element(by.id('address')).sendKeys('Lot 8919, Jalan Lembu');
@@ -119,15 +140,31 @@ var TutorRegisterSubject = function (name, email){
 
   browser.wait(urlChanged2, 5000);
 
-  expect(element(by.css('.alert')).getText()).toContain('Successfully');
+  // expect(element(by.css('.alert')).getText()).toContain('Successfully');
+
+  browser.wait(function() {
+    //wait for another 30 seconds
+    return element(by.id('tuitionservicepanel')).isPresent();
+  }, 10000);
 
   // Check for all the list of the subjects
   element(by.id('tuitionservicepanel')).click();
 
+  browser.wait(function() {
+    //wait for another 30 seconds
+    return element(by.id('subjects')).isPresent();
+  }, 10000);
+
+  browser.takeScreenshot().then(function (png) {
+          writeScreenShot(png, 'tuitionservicepanel.png');
+      });
+
   expect(element(by.id('subjects')).getText()).toContain('Bahasa Malaysia (Standard 1-3)');
   expect(element(by.id('subjects')).getText()).toContain('Bahasa Malaysia (Standard 4-6)');
   console.log("Subject Registered");
-}
+  deferred.fulfill(true);
+  return deferred.promise;
+};
 
 //module.exports.TutorRegisterSubject = TutorRegisterSubject;
 
@@ -136,4 +173,4 @@ module.exports = {
   RegisterStudent,
   RegisterTutor,
   TutorRegisterSubject
-}
+};
